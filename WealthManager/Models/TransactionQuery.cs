554fa35e13599a474d2f9e59186e0b592df13e8e
vroyibg <vroyibg@gmail.com>
2020-06-06 17:20:57 +0700

    add date range filter

--- WealthManager/Models/TransactionQuery.cs
+++ WealthManager/Models/TransactionQuery.cs
@@ -1,8 +1,12 @@
 namespace WealthManager.Models
 {
+    using System;
+
     public class TransactionQuery
     {
         public int? WalletId { get; set; }
         public int? CategoryId { get; set; }
+        public DateTime? DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
     }
 }
