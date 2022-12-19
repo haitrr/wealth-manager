1b27f5a10302d430b71fe888fb88fa19d4ee6b0a
haitrr <h@hai.fyi>
2022-12-20 00:44:23 +0700

    redirect to login page

--- web/wealth-manager/pages/index.tsx
+++ web/wealth-manager/pages/index.tsx
@@ -1,11 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import {useContext, useEffect} from "react";
+import AuthenticationContext from "../contexts/AuthenticationContext";
+import {useRouter} from "next/router";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const {isAuthenticated} = useContext(AuthenticationContext);
+  const router = useRouter();
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login");
+    }
+  }, [router, isAuthenticated]);
+
   return (
     <>
       <Head>
@@ -13,7 +20,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>test</div>
     </>
-  )
+  );
 }
