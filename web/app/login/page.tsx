b4c74ccba85ac756899f3317caeb6925069cdaef
Hai Tran <vroyibg@gmail.com>
2023-09-05 21:52:55 +0700

    login form

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -1,8 +1,56 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default function Login() {
-    return (
+export default function LoginPage() {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Here, you can add your login logic.
+    // For simplicity, we'll just redirect to the home page on submit.
+    router.push('/');
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
         <div>
-        <h1>Login</h1>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
         </div>
-    )
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
