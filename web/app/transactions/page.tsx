46cbb225b7c5bb55a4316623c94034efb0e1fbd7
Hai Tran <vroyibg@gmail.com>
2023-09-17 14:06:40 +0700

    transaction page

---
+++ web/app/transactions/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Transactions from "@/components/transactions"
+
+const TransactionsPage = () => {
+    return <div>
+        <h1>Transactions</h1>
+        <Transactions/>
+    </div>
+}
+
+export default TransactionsPage;
