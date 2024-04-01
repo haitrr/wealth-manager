ba0c37f26a0414dfc86f8b7eac4288d87d21151e
haitrr <h@hai.fyi>
2024-04-02 00:15:31 +0700

    fix lint

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 import prisma from "../lib/prisma";
+import { Decimal } from "@prisma/client/runtime/library";
 
-const formatVND = (amount) => {
+const formatVND = (amount: number) => {
   return new Intl.NumberFormat("vi-VN", {
     style: "currency",
     currency: "VND",
@@ -22,13 +23,12 @@ export default async function Home({}) {
   const [{balance}] = await prisma.$queryRaw`SELECT 
   (SELECT COALESCE(SUM(value), 0) FROM "Transaction" WHERE "categoryId" IN (SELECT id FROM "Category" WHERE "type" = 'INCOME')) - 
   (SELECT COALESCE(SUM(value), 0) FROM "Transaction" WHERE "categoryId" IN (SELECT id FROM "Category" WHERE "type" = 'EXPENSE')) AS balance;
-`
-console.log(balance)
+` as {balance: Decimal}[];
   return (
     <div>
       <div>Wealth Manager</div>
       <div>Balance</div>
-      <div>{formatVND(balance)}</div>
+      <div>{formatVND(balance.toNumber())}</div>
       <div>Transactions</div>
       {transactions.map((transaction) => (
         <div
@@ -36,7 +36,7 @@ console.log(balance)
           key={transaction.id}
         >
           <div>{dayjs(transaction.date).fromNow()}</div>
-          <div>{formatVND(transaction.value)}</div>
+          <div>{formatVND(transaction.value.toNumber())}</div>
           <div>{transaction.category.name}</div>
         </div>
       ))}
