59f8a152972df9badfe1640dc8dc26b566022e01
haitrr <h@hai.fyi>
2024-04-20 20:48:22 +0700

    navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <NavigationBar/>
     </html>
+  );
+}
+
+function NavigationBar() {
+  return (
+    <nav className="flex sticky bottom-0 justify-between p-2 bg-gray-900 text-white">
+      <div>Home</div>
+      <div>Add</div>
+      <div>Budget</div>
+      <div>Settings</div>
+    </nav>
   );
 }
