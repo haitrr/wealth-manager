dba3cac64c5afe29437ba71b94a8e7db13d58195
haitrr <h@hai.fyi>
2024-04-20 21:17:51 +0700

    basic navigation bar

---
+++ app/NavigationBar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import {usePathname} from "next/navigation";
+
+const menuItems = [
+  {label: "Home", href: "/"},
+  {label: "Budgets", href: "/budgets"},
+  {label: "Settings", href: "/settings"},
+];
+
+type Props = {
+  item: {
+    label: string;
+    href: string;
+  };
+};
+
+function MenuItem({item}: Props) {
+  const pathName = usePathname();
+  const isActive = pathName === item.href;
+  const textColor = isActive ? "text-blue-500" : "text-gray-500";
+  console.log(item, textColor);
+  return (
+    <div className={`${textColor}`}>
+      <a href={item.href}>{item.label}</a>
+    </div>
+  );
+}
+
+export function NavigationBar() {
+  return (
+    <nav className="flex sticky bottom-0 justify-between p-2 bg-gray-900 text-white">
+      {menuItems.map((item) => {
+        return <MenuItem key={item.href} item={item} />;
+      })}
+    </nav>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import {NavigationBar} from "./NavigationBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Wealth Manager",
   icons: {
-    icon: "/icon.jpeg"
+    icon: "/icon.jpeg",
   },
 };
 
@@ -19,18 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <NavigationBar/>
+      <NavigationBar />
     </html>
-  );
-}
-
-function NavigationBar() {
-  return (
-    <nav className="flex sticky bottom-0 justify-between p-2 bg-gray-900 text-white">
-      <div>Home</div>
-      <div>Add</div>
-      <div>Budget</div>
-      <div>Settings</div>
-    </nav>
   );
 }
