cbe8821701904f9a2e54bcf446bebed785ef543d
haitrr <h@hai.fyi>
2024-04-20 21:39:44 +0700

    budget list

--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -1,7 +1,23 @@
-export default function BudgetPage() {
+import prisma from "@/lib/prisma";
+import { Money } from "../Money";
+
+export default async function BudgetPage() {
+  const budgets = await prisma.budget.findMany();
   return (
-    <div>
-      <h1>Budgets</h1>
+    <div className="p-2 ">
+      <div className="flex justify-center">
+        <h1>Budgets</h1>
+      </div>
+      <div >
+        {budgets.map((budget) => {
+          return (
+            <div className="flex justify-between" key={budget.id}>
+              <div>{budget.name}</div>
+              <Money value={budget.value.toNumber()} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
