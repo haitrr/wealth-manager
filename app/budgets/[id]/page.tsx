f1747e5fc8ade6e45b4e01232f98a5934f0b41ac
haitrr <h@hai.fyi>
2024-05-04 11:39:10 +0700

    adjust font size

--- app/budgets/[id]/page.tsx
+++ app/budgets/[id]/page.tsx
@@ -43,7 +43,7 @@ const dayLeft = dayjs(endDate).diff(dayjs(), "day");
 
   return (
     <div>
-      <div className="flex p-4 flex-col gap-1 justify-center text-lg items-center">
+      <div className="flex p-4 flex-col gap-1 justify-center text-sm items-center">
         <div>{budget.name}</div>
         <Money value={budget.value.toNumber()} />
         <div className="flex justify-between w-full">
@@ -57,7 +57,7 @@ const dayLeft = dayjs(endDate).diff(dayjs(), "day");
           </div>
         </div>
         <BudgetProgress budget={budget} />
-      <div className="flex justify-start w-full flex-col">
+      <div className="flex justify-start w-full flex-col text-sm pt-1">
         <div>{`${dayjs(startDate).format("DD/MM")} - ${dayjs(endDate).format("DD/MM")}`}</div>
         <div>{`${dayLeft} days left`}</div>
       </div>
