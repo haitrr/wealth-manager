d8a972794e340a82e5f8acc10284122d92318e89
haitrr <h@hai.fyi>
2025-05-25 22:35:39 +0700

    improve navigation bar

--- app/NavigationBar.tsx
+++ app/NavigationBar.tsx
@@ -1,11 +1,12 @@
 "use client";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const menuItems = [
-  {label: "Home", href: "/", pattern: /^\/$/},
-  {label: "Budgets", href: "/budgets", pattern: /^\/budgets.*/},
-  {label: "Loans", href: "/debt-loan", pattern: /^\/debt-loan.*/},
-  {label: "Settings", href: "/settings", pattern: /^\/settings.*/},
+  {label: "Home", href: "/", pattern: /^\/$/,  icon: "🏠"},
+  {label: "Budgets", href: "/budgets", pattern: /^\/budgets.*/, icon: "📊"},
+  {label: "Loans", href: "/debt-loan", pattern: /^\/debt-loan.*/, icon: "💳"},
+  {label: "Settings", href: "/settings", pattern: /^\/settings.*/, icon: "⚙️"},
 ];
 
 type Props = {
@@ -13,26 +14,39 @@ type Props = {
     label: string;
     href: string;
     pattern: RegExp;
+    icon: string;
   };
 };
 
 function MenuItem({item}: Props) {
   const pathName = usePathname();
   const isActive = pathName.match(item.pattern);
-  const textColor = isActive ? "text-blue-500" : "text-gray-500";
   return (
-    <div className={`${textColor}`}>
-      <a href={item.href}>{item.label}</a>
-    </div>
+    <Link 
+      href={item.href} 
+      className={`transition-all duration-300 px-4 py-2 rounded-lg flex flex-col items-center relative ${
+        isActive 
+          ? "text-blue-600 font-medium" 
+          : "text-gray-500 hover:text-blue-500"
+      }`}
+    >
+      <span className="text-xl">{item.icon}</span>
+      <span className="text-xs mt-1">{item.label}</span>
+      {isActive && <span className="absolute -bottom-0.5 w-1/2 h-1 bg-blue-500 rounded-full"></span>}
+    </Link>
   );
 }
 
 export function NavigationBar() {
   return (
-    <div className="flex justify-between p-4 text-xl bg-secondary">
-      {menuItems.map((item) => {
-        return <MenuItem key={item.href} item={item} />;
-      })}
-    </div>
+    <nav className="fixed bottom-0 left-0 right-0 z-10 flex justify-center p-1 border-t border-secondary bg-secondary backdrop-blur-md shadow-sm">
+      <div className="container flex items-center justify-between max-w-5xl px-2">
+        <div className="flex items-center justify-between w-full">
+          {menuItems.map((item) => (
+            <MenuItem key={item.href} item={item} />
+          ))}
+        </div>
+      </div>
+    </nav>
   );
 }
