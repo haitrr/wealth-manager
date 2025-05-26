57dcd8d9b8f169f1ee1e0ba62df7502bf5d03c82
haitrr <h@hai.fyi>
2025-05-26 17:02:41 +0700

    fix: cache

--- server-actions/transaction.ts
+++ server-actions/transaction.ts
@@ -1,10 +1,13 @@
 "use server"
 
 import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export default async function createTransaction(transaction: any) {
     const newTransaction = await prisma.transaction.create({
         data: transaction,
     });
+    revalidatePath("/transactions");
+    revalidatePath("/");
     return newTransaction;
 }
